9c0dc0c0a8bae70ce18e18e5e2a60028210ffed6
stephanecaron <stephane.caron@groupesavoie.com>
2023-01-18 17:42:55 -0400

    delete backend stuff

--- index.js
+++ index.js
@@ -2,20 +2,15 @@ const express = require('express')
                 require('dotenv').config()
 const res = require('express/lib/response')
 const data = require('./db.json')
-const characters = require('./characterList.json')
 const cors = require('cors')
 const { restart } = require('nodemon')
 const port = process.env.PORT
 const app = express()
-const fs = require('fs')
-const fileName = './db.json'
-const file = require(fileName);
 const dbcall = require('./dbcall.js');
-const { json } = require('express/lib/response')
 
 
 app.use(cors({origin:"http://localhost:3000",credentials:true}))
-app.use(express.json())
+/* app.use(express.json()) */
 
 app.get('/get', (req, res) => {
     console.log(req.query)
@@ -31,7 +26,7 @@ app.get('/getcharacters', (req, res) => {
 });
 
 app.post('/post', (req, res) => {
-    let player= JSON.parse(req.body.data)
+    let player= req.body.data
     console.log (player.playerNameValue + " " + player.playerCharacterValue + " " + player.playerSeedValue)
     dbcall.query(`INSERT INTO milkaggdb (playerNameValue, playerCharacterValue, playerSeedValue) VALUES ('${player.playerNameValue}', '${player.playerCharacterValue}', '${player.playerSeedValue}');`, function(error, result) {
     if (error) throw error;
@@ -45,15 +40,15 @@ app.get('/getall', (req, res) => {
     dbcall.query(`SELECT * FROM milkaggdb`, function (error, result) {
         if (error) throw error;
         res.status(200).json(result)
-        console.log (result)
+        console.log ('sent all players to user')
     })
 })
 
-app.delete('/delete'), (req, res) => {
+app.delete('/delete', (req, res, err) => {
     console.log (req.query.id)
-/*     dbcall.query(`DELETE FROM milkaggdb WHERE id=${req.query.id};`) */
-    if (error) throw error;
-}
+    dbcall.query(`DELETE FROM milkaggdb WHERE id=${req.query.id};`)
+    if (err) throw err;
+})
 
 /*  app.delete('/delete', (req, res) => {
     const result = data.playerList.filter(player => player.id !== Number(req.query.id));
